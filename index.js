5e9a086e6a0e2d8c0e140aec14c1c055bba656ad
Neo <unieo@aol.com>
2021-01-25 10:16:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 client.on("ready", () => {
     // Logging that the bot is online
     console.log("Bot is online!");
+    // This is the activity/status settings...
     client.user.setActivity("Bot message here!" 
     {
         type: "PLAYING", // Can be STREAMING, WATCHING, etc.
