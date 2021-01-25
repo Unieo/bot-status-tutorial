956599c26278b8c153915ef8514b4b6907669fd0
Neo <unieo@aol.com>
2021-01-25 10:15:22 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+
+// When the bot is ready.
+client.on("ready", () => {
+    // Logging that the bot is online
+    console.log("Bot is online!");
+    client.user.setActivity("Bot message here!" 
+    {
+        type: "PLAYING", // Can be STREAMING, WATCHING, etc.
+    });
+});
