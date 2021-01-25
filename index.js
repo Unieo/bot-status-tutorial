e1dc0741fbd51fc54bb84075274263d8c560e1d1
Neo <unieo@aol.com>
2021-01-25 10:35:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 // When the bot is ready.
 client.on("ready", () => {
     // Logging that the bot is online
@@ -9,3 +8,18 @@ client.on("ready", () => {
         type: "PLAYING", // Can be STREAMING, WATCHING, etc.
     });
 });
+
+// Or
+
+// When the bot is ready.
+client.on("ready", () => {
+    // Logging that the bot is online
+    console.log("Bot is online!");
+    // Set the client user's presence
+    client.user.setPresence({
+    activity: {
+        name: 'with discord.js'
+        }, status: 'idle'
+    });
+});
+
